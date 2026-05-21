40760b95b7c8fd9bba8594994341c0b15c75ab58
SpecialRoyalty <nataliatupalskaparis@gmail.com>
2026-05-22 00:05:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1533,8 +1533,8 @@ async def save_user_message_if_session(update: Update):
 # PUBLIC MESSAGES V24
 # =========================
 
-MSG_PARTICIPATION_REQUIRED = "⚠️ Merci de participer avant d’envoyer un message.\nEnvoyez au moins 1 photo ou 1 vidéo jamais publiée."
-MSG_REPOST = "♻️ Ce média a déjà été publié."
+MSG_PARTICIPATION_REQUIRED = "⚠️ Merci de participer avant d’envoyer un message.\nEnvoyez au moins 1 photo ou 1 vidéo FR jamais publiée."
+MSG_REPOST = "♻️ Ce média a déjà été publié.Merci d'envoyez un média FR et surtout qui n'a pas tourné 328371 fois"
 MSG_LINK_FORBIDDEN = "🔗 Les liens ne sont pas autorisés."
 MSG_FORWARD_FORBIDDEN = "🚫 Les transferts ne sont pas autorisés."
 MSG_GENERIC_FORBIDDEN = "🚫 Message non autorisé."
