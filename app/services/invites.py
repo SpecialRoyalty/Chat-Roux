8069d6140d158b3fcb05d333eb8f50b0b17847ec
SpecialRoyalty <nataliatupalskaparis@gmail.com>
2026-07-02 21:42:23 +0200

    Add files via upload

--- app/services/invites.py
+++ app/services/invites.py
@@ -54,7 +54,7 @@ async def invite_kb():
     url=f'https://t.me/{username}?start=invite' if username else None
     if url:
         return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='🔗 Recevoir mon lien', url=url)]])
-    return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='🔗 Recevoir mon liens', callback_data='invite_private')]])
+    return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='🔗 Recevoir mon lien', callback_data='invite_private')]])
 
 async def send_invite_ad(bot:Bot, force:bool=False):
     if not force and not await st.is_open(): return None
